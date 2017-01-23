7ea05618d2afc294daf81882eaee3ed2db862ad0
Omar Erminy <omrsin@gmail.com>
2017-01-23 15:56:07 +0100

    Extends the spark library model
    Now, map and reduce methods on JavaRDD objects are also modeled.

--- spark-java/src/main/java/de/tudarmstadt/thesis/jpf/spark/SparkExample.java
+++ spark-java/src/main/java/de/tudarmstadt/thesis/jpf/spark/SparkExample.java
@@ -7,6 +7,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 
 public class SparkExample {
 	
@@ -19,7 +20,7 @@ public class SparkExample {
 		
 		JavaSparkContext spark = new JavaSparkContext(conf);
 		
-		List<Integer> numberList = Arrays.asList(1,2,3,4);
+		List<Integer> numberList = Arrays.asList(1,2,3,3,4);
 		JavaRDD<Integer> numbers = spark.parallelize(numberList);		
 		JavaRDD<Integer> filteredNumbers = numbers.filter(new Function<Integer, Boolean>() {
 			
@@ -30,8 +31,27 @@ public class SparkExample {
 				return v1 > 1 && v1 <=3;
 			}
 		});
+		JavaRDD<Integer> mappedNumbers = filteredNumbers.map(new Function<Integer, Integer>() {
+
+			@Override
+			public Integer call(Integer v1) throws Exception {
+				if(v1 == 2) {
+					return 7;
+				} else {
+					return v1;
+				}
+			}			
+		});
 		
-		System.out.println(filteredNumbers);
+		Integer numbersSum = mappedNumbers.reduce(new Function2<Integer, Integer, Integer>() {			
+			@Override
+			public Integer call(Integer v1, Integer v2) throws Exception {				
+				return v1+v2;
+			}
+		});
+		
+		System.out.println(mappedNumbers.collect());
+		System.out.println(numbersSum);
 		
 		spark.stop();
 		spark.close();
